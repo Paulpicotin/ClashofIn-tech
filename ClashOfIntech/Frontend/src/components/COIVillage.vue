5d3f7d72f1bcde03f3995d485002d14b4156ee68
Paulpicotin <44963927+Paulpicotin@users.noreply.github.com>
2020-05-15 09:17:57 +0200

    village et bouton effectif

--- ClashOfIntech/Frontend/src/components/COIVillage.vue
+++ ClashOfIntech/Frontend/src/components/COIVillage.vue
@@ -7,15 +7,15 @@
     <label class="ressource" for = "BLE">BLE</label>
     <label class="ressource" for = "PIERRE">PIERRE</label>
     <router-link to="./">
-    <button v-on:click ="param" >{{'PARAM'}}</button></router-link>
+    <button class="bouton" v-on:click ="param" >{{'PARAM'}}</button></router-link>
     <router-link to="./">
-    <button v-on:click ="attaque" >{{'ATTAQUE'}}</button></router-link> 
+    <button class="bouton" v-on:click ="attaque" >{{'ATTAQUE'}}</button></router-link> 
     <router-link to="./">
-    <button v-on:click ="defense" >{{'DEFENSE'}}</button></router-link>  
+    <button class="bouton" v-on:click ="defense" >{{'DEFENSE'}}</button></router-link>  
     <router-link to="./">
-    <button v-on:click ="batiment" >{{'BATIMENT'}}</button></router-link>  
+    <button class="bouton" v-on:click ="batiment" >{{'BATIMENT'}}</button></router-link>  
     <router-link to="./">
-    <button v-on:click ="ameliorer" >{{'AMELIORER'}}</button></router-link>       
+    <button class="bouton" v-on:click ="ameliorer" >{{'AMELIORER'}}</button></router-link>
     
   </div>
 </template>
@@ -34,5 +34,16 @@ input {
     margin-bottom: 10px;
     width: 150px;
     margin-left: 7%;
+}
+.bouton{
+  background: #6AB6D8;
+  padding: 25px;
+  margin-top: 50%;
+  margin-left: 150px;
+  border: 3px solid #2E86BB;
+  color: white;
+  font-size: 20px;
+  text-align: center;
+  position: relative
 }
 </style>
